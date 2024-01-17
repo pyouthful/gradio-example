bac799700b702c4feeecf1766f7c0bde3c013fab
Shihao Qiu <156822685+pyouthful@users.noreply.github.com>
2024-01-17 14:06:11 +0000

    Add Dockerfile

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ def greet(name):
 demo = gr.Interface(fn=greet, inputs="textbox", outputs="textbox")
     
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0",
+                server_port=7860)
