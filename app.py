70d8d582fbb5a1229e1a7b5789cdfcfcb1fa7632
Shihao Qiu <156822685+pyouthful@users.noreply.github.com>
2024-01-21 09:41:21 +0000

    add env config

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+import os
+
+try:
+    server_port = int(os.environ.get('SERVER_PORT', ''))
+except ValueError:
+    server_port = 7860
+
+root_path = os.environ.get('ROOT_PATH', '')
+
 import gradio as gr
 
 def greet(name):
@@ -7,4 +16,5 @@ demo = gr.Interface(fn=greet, inputs="textbox", outputs="textbox")
     
 if __name__ == "__main__":
     demo.launch(server_name="0.0.0.0",
-                server_port=7860)
+                server_port=server_port,
+                root_path=root_path)
